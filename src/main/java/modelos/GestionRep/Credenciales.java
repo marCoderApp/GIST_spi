b21287b0d4d19c814b8f85c2ab1f7ac23d853090
Marcos <marCoderApp>
2025-10-12 13:19:21 -0300

    Agregadas clases de vista y clase Credenciales

---
+++ src/main/java/modelos/GestionRep/Credenciales.java
@@ -0,0 +1,28 @@
+package modelos.GestionRep;
+
+public class Credenciales {
+
+	   private String usuario;
+	    private String contrasena;
+
+	    public Credenciales(String usuario, String contrasena) {
+	        this.usuario = usuario;
+	        this.contrasena = contrasena;
+	    }
+
+	    public String getUsuario() {
+	        return usuario;
+	    }
+
+	    public void setUsuario(String usuario) {
+	        this.usuario = usuario;
+	    }
+
+	    public String getContrasena() {
+	        return contrasena;
+	    }
+
+	    public void setContrasena(String contrasena) {
+	        this.contrasena = contrasena;
+	    }
+}
