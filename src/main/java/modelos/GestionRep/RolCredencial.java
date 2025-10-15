e46eba9605185909970a8519c50210d99042dc81
Marcos <marCoderApp>
2025-10-15 08:27:22 -0300

    Añadido Enum de rol de credenciales

---
+++ src/main/java/modelos/GestionRep/RolCredencial.java
@@ -0,0 +1,25 @@
+package modelos.GestionRep;
+
+public enum RolCredencial {
+	ADMIN("ADMIN"),
+    TECNICO("TECNICO");
+
+    private final String valor;
+
+    RolCredencial(String valor) {
+        this.valor = valor;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public static RolCredencial fromString(String valor) {
+        for (RolCredencial e : RolCredencial.values()) {
+            if (e.getValor().equalsIgnoreCase(valor)) {
+                return e;
+            }
+        }
+        throw new IllegalArgumentException("Estado no válido: " + valor);
+    }
+}
