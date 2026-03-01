30b0353d4c7a775873000d1951b001fe09854530
marCoderApp <farbusfer98@gmail.com>
2026-03-01 01:16:38 -0300

    CHANGES IN DB PROPERTIES AND GIT IGNORE AND CONEXSON CLASS

--- src/main/java/conexion/ConexionDB.java
+++ src/main/java/conexion/ConexionDB.java
@@ -1,45 +1,43 @@
 package conexion;
 
 import javafx.scene.control.Alert;
-
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
-
 
 public class ConexionDB {
 
-	private static final String URL = "jdbc:mysql://localhost:3306/bd_gist";
-	private static final String USER = "root";
-	private static final String PASSWORD = "pass123";
-	
 	public static Connection conectar() {
-		Connection conexion = null;
-		try {
-			Properties props = new Properties();
-			InputStream input = ConexionDB.class
-					.getClassLoader()
-					.getResourceAsStream("db.properties");
 
+		try (InputStream input = ConexionDB.class
+				.getClassLoader()
+				.getResourceAsStream("db.properties")) {
+
+			if (input == null) {
+				throw new RuntimeException("No se encontró db.properties");
+			}
+
+			Properties props = new Properties();
 			props.load(input);
 
-			conexion = DriverManager.getConnection(
+			Connection conexion = DriverManager.getConnection(
 					props.getProperty("db.url"),
 					props.getProperty("db.user"),
 					props.getProperty("db.password")
 			);
 
 			System.out.println("Conexión exitosa");
+			return conexion;
+
 		} catch (Exception e) {
+
 			Alert alert = new Alert(Alert.AlertType.ERROR);
 			alert.setHeaderText("Error de conexión");
 			alert.setContentText("Verifique que MySQL esté instalado y la base creada.");
 			alert.showAndWait();
-		}
 
-		return conexion;
+			throw new RuntimeException("Error al conectar a la base de datos", e);
+		}
 	}
-	
 }
